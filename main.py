1badb4e274fbaf33fbf615e5d615663dd403300c
Wasmae <mwaeas@gmail.com>
2021-10-30 12:25:49 -0700

    exception handling and css generation

--- main.py
+++ main.py
@@ -2,17 +2,48 @@ import argparse
 import os
 from page import Page
 
+import sys
+
+class recursionlimit:
+    def __init__(self, limit):
+        self.limit = limit
+        self.old_limit = sys.getrecursionlimit()
+
+    def __enter__(self):
+        sys.setrecursionlimit(self.limit)
+
+    def __exit__(self, type, value, tb):
+        sys.setrecursionlimit(self.old_limit)
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--link', help="URL of CHYOA start", type=str)
+parser.add_argument('--links', help="Multiple URLS", type=str)
 parser.add_argument('--images', help="Download images", type=bool, default=True)
 parser.add_argument('--directory','-d', help="Directory to store downloaded files", default=os.getcwd(), type=str)
 
 args = vars(parser.parse_args())
 
-print("Collecting Links")
-page = Page(args['link'], "", args['directory'], args['images'])
-print("Links Collected")
-print("Building HTML Files")
-page.createHTML()
-print("Download Complete")
+
+
+
+
+with recursionlimit(30000):
+    print(sys.getrecursionlimit())
+    if args['links']:
+        links = []
+        for i in args['links'].split(","):
+            print("Collecting Links From " + i)
+            page = Page(i, "", args['directory'], args['images'])
+            print("Links Collected")
+            print("Building HTML Files")
+            page.createHTML()
+            print("Download Complete")
+        print("All Files Downloaded")
+    else:
+        print("Collecting Links")
+        page = Page(args['link'], "", args['directory'], args['images'])
+        print("Links Collected")
+        print("Building HTML Files")
+        page.createHTML()
+        print("Download Complete")
--- page.py
+++ page.py
@@ -5,8 +5,9 @@ import unicodedata
 import re
 import shutil
 
+
 class Page:
-    def __init__(self, url, name, dir, downloadImages, filename="index", root=None, parent=None):
+    def __init__(self, url, name, dir, downloadImages, filename="index", root=None, parent=None, id=0, propagate=True):
         self.root = root
         self.dir = dir
         self.content = BeautifulSoup(requests.get(url).text,'html.parser')
@@ -18,58 +19,105 @@ class Page:
             self.traversed = []
             self.dir = dir + "/" + self.content.find("h1").text
             self.pageCurrent = 0
+            os.makedirs(self.dir+"/images", exist_ok=True)
+            os.makedirs(self.dir+"/chapters", exist_ok=True)
+            self.known = {}
 
         self.parent = parent
         self.url = url
         self.name = name
+        self.id = id
+        self.propagate = propagate
 
-        self.root.pageCurrent += 1
-        print(str(self.root.pageCurrent) + " Links Scraped")
+        if propagate:
+            self.root.pageCurrent += 1
+            print(str(self.root.pageCurrent) + " Links Scraped" + " | Parent chain: " + self.recurseParents())  
 
-        self.getChildren()
+            self.root.known[url] = filename
+            self.getChildren()
 
-        os.makedirs(self.dir+"/images", exist_ok=True)
-        os.makedirs(self.dir+"/chapters", exist_ok=True)
-    
+
+
+    def recurseParents(self):
+        return ""
+        ''' current = self.parent
+        ids = [self.id]
+        while current != None:
+            ids.append(current.id)
+            current = current.parent
+        retval = ""
+        for i in ids:
+            retval = retval + str(i) + " to "
+        return retval[:-3]'''
+
+    def __str__(self):
+        retval = "Children: \n"
+        try:
+            for i in self.children:
+                retval += "------" + str(i)
+        except AttributeError:
+            pass
+        return retval
+
+
     def getChildren(self):
         self.children = []
         temp = self.content.find("div",class_="question-content")
         links = BeautifulSoup(str(temp), 'html.parser')
         #Create new Page for each link in div
+        count = 1
         for i in links.find_all("a", class_=""):
             href = i['href']
 
-            child = Page(href,i.text, self.dir, self.downloadImages,filename=self.name+"-"+i.text,root=self.root, parent=self)
+            if (not (href in self.root.known.keys())):
+                child = Page(href,i.text, self.dir, self.downloadImages,filename=self.name+"-"+i.text,root=self.root, parent=self, id=self.id+count)
+            else:
+                child = Page(href,i.text, self.dir, self.downloadImages,filename=self.root.known.get(href),root=self.root, parent=self, id=self.id+count, propagate=False)
+
             self.children.append(child)
+            count+= 1
     
     def createHTML(self):
+        if not self.propagate:
+            return
+
         #Base HTML
-        html = BeautifulSoup("<!DOCTYPE html><html><head></head><body></body></html>", 'html.parser')
-        
+        with open('default.html', 'r') as f:
+            html = BeautifulSoup(f.read(), 'html.parser')
+
         if(self.downloadImages):
             if(self.root == self):
-                coverImage = self.content.find("div",class_="cover").find("img")
-                if(coverImage):
-                    savePath = self.dir + "/images/" + self.slugify(coverImage['alt'])
-                    self.saveImage(coverImage['src'],savePath)
-                    coverImage['src'] = savePath
-            
-            contentImages = self.content.find('div',class_="chapter-content").find_all('img')
+                try:
+                    coverImage = self.content.find("div",class_="cover").find("img")
+                    if(coverImage):
+                        name = self.slugify(coverImage['alt'])
+                        savePath = self.dir + "/images/" + name
+                        self.saveImage(coverImage['src'],savePath)
+                        coverImage['src'] = "./images/" + name if self.root == self else "../images/" + name
+                except AttributeError:
+                    pass
+            try:
+                contentImages = self.content.find('div',class_="chapter-content").find_all('img')
+            except AttributeError:
+                contentImages = []
+
             for i in range(len(contentImages)):
                 image = contentImages[i]
                 name = self.slugify(str(i)+self.name)
-                savePath = self.dir + "/images/" + name
-                self.saveImage(image['src'],savePath)
+                savePath = "./images/" + name if self.root == self else "../images/" + name
+                imgPath = image['src']
+                try:
+                    self.saveImage(imgPath, self.dir + "/images/" + name)
+                except (requests.exceptions.InvalidSchema) as err:
+                    pass
                 image['src'] = savePath
-    
 
-        #Add CSS
         head = html.find("head")
-        stylesheet = html.new_tag("style")
-        stylesheet.string=open(os.getcwd()+"/default.css").read()
-        head.append(stylesheet)
         
+        stylesheet = [link for link in html.findAll("link") if "style.css" in link.get("href", [])][0]
+
         body = html.find("body")
+
 
         #Copy Story/Chapter header from chyoa.com
         try:
@@ -83,17 +131,24 @@ class Page:
             except AttributeError:
                 header = html.new_tag('h1')
                 header.string = self.name
+
         body.append(header)
 
         #Copy main content from chyoa.com
-        mainContent = self.content.find("div",class_="chapter-content")
-        body.append(mainContent)
+        try:
+            mainContent = self.content.find("div",class_="chapter-content")
+            body.append(mainContent)
+        except ValueError:
+            print("Unable to fetch content at " + self.url)
 
         #Div container for choice links
         linkContainer = html.new_tag('div')
         linkContainer['class'] = "linkContainer"
 
-        body.append(self.content.find("header", class_="question-header"))
+        try:
+            body.append(self.content.find("header", class_="question-header"))
+        except ValueError:
+            pass
 
         body.append(linkContainer)
 
@@ -107,7 +162,7 @@ class Page:
         for i in self.children:
             link = html.new_tag("a")
             link.string = i.name
-            link['href'] = i.dir+"/chapters/"+i.filename 
+            link['href'] = "./chapters/"+i.filename if self.root == self else i.filename 
             link['class'] = "chapterLinks"
             linkContainer.append(link)
 
@@ -119,7 +174,7 @@ class Page:
         #Link to return to previous page
         try:
             back.string = "Previous Chapter"
-            back['href'] = self.dir+"/index.html" if self.parent==self.root else self.dir+"/chapters/"+self.parent.filename
+            back['href'] = "../index.html" if self.parent==self.root else self.parent.filename
             back['class'] = "styledLink prev"
             persistent.append(back)
         except AttributeError:
@@ -136,7 +191,7 @@ class Page:
         restart = html.new_tag("a")
         restart.string = "Restart"
         restart['class'] = "styledLink prev"
-        restart['href'] = self.dir+"/index.html"
+        restart['href'] = "../index.html"
 
         #Add subelements to linkContainer
         persistent.append(restart)
@@ -147,16 +202,39 @@ class Page:
         if self.root == self:
             #index.html file in root directory
             published = open(self.dir+"/index.html", 'w', encoding='utf-8')
+
+            stylesheet['href'] = "chapters/style.css"
+
+            css = open(self.dir+"/chapters/style.css", 'w', encoding='utf-8')
+            with open('default.css', 'r') as f:
+                css.write(f.read())
+                css.close()
         else:
             #[chapter name].html file in 'chapters' directory
             published=open(self.dir+'/chapters/'+self.filename, 'w', encoding="utf-8")
         published.write(str(html.prettify()))
+        published.close()
 
         #Create HTML for child Pages
         [i.createHTML() for i in self.children]
 
     def saveImage(self, url, file):
-        r = requests.get(url, stream = True)
+        try:
+            r = requests.get(url, stream = True)
+        except requests.exceptions.MissingSchema:
+            r = requests.get("http://" + url, stream=True)
+        except requests.exceptions.SSLError:
+            print("Unable to download image from " + url)
+            return
+        except requests.exceptions.TooManyRedirects:
+            print("Unable to download image from " + url)
+            return
+        except requests.exceptions.ConnectionError:
+            print("Unable to download image from " + url)
+            return
+        except requests.exceptions.InvalidURL:
+            print(url + " is invalid url")
+            return
 
         # Check if the image was retrieved successfully
         if r.status_code == 200:
@@ -167,7 +245,7 @@ class Page:
             with open(file,'wb') as f:
                 shutil.copyfileobj(r.raw, f)
         else:
-            raise ValueError("Unable to download image")
+            print("Unable to download image from " + url)
 
     #Function to convert page names into valid file paths
     def slugify(self, value, allow_unicode=False):
@@ -184,7 +262,12 @@ class Page:
         else:
             value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
         value = re.sub(r'[^\w\s-]', '', value.lower())
-        return re.sub(r'[-\s]+', '-', value).strip('-_')
+        thing = re.sub(r'[-\s]+', '-', value).strip('-_')
+        if len(thing) > 248:
+            thing = thing[::248]
+        return thing
+
+
 
 
 
